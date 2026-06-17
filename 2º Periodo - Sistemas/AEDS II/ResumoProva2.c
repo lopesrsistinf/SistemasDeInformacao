cad7211cc3d596a04ee0289847f3a280cb536781
RamonNotelinux <lopesrsistinf@gmail.com>
2026-06-17 19:51:52 -0300

    17 jun 26

--- 2º Periodo - Sistemas/AEDS II/ResumoProva2.c
+++ 2º Periodo - Sistemas/AEDS II/ResumoProva2.c
@@ -1,12 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void organizaSelecao(int VET[],int n){
-    int i,j,menor,aux;
-    for(j= 0; j < n-1; j++){
-        
-    }
+struct abb{
+    int Info;
+    struct abb *Esq,*Dir;
+};
+typedef struct abb Abb;
+
+Abb*CriarArv(){
+    return NULL;
 }
+
+Abb*InserirNo(Abb*a,int v){
+    if (a == NULL){
+        Abb*Novo = (Abb*)malloc(sizeof(Abb));
+        Novo -> Info = v;
+        Novo -> Esq = Novo -> Dir = NULL;
+        return Novo;
+    }
+    if (v < a -> Info) a->Esq = InserirNo(a->Esq,v);
+    else if (v > a -> Info) a -> Dir = InserirNo(a ->Dir,v);
+    return a;
+}
+
+void ImprimirNo(Abb*a){
+    if (a == NULL) return;
+    ImprimirNo(a -> Esq);
+    printf("%d ",a->Info);
+    ImprimirNo(a -> Dir);
+}
+
+Abb*BuscaNo(Abb*a,int v){
+    if (a == NULL) return NULL;
+    if (v == a -> Info) return a;
+    if (v < a->Info) return BuscaNo(a->Esq,v);
+    else return BuscaNo(a -> Dir,v);
+}
+
+Abb*ApagarNo(Abb*a,int v){
+    
+}
+
 
 int main(){
 
